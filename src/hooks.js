594864f22e2b8b99c2418324d8af2a2abe8e62df
cctipmao <cctip_mao@proton.me>
2023-10-30 14:00:16 +0800

    fix: shared data server

--- src/hooks.js
+++ src/hooks.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { getSharedData, watchSharedData } from './clientStorage';
 
 export const useSharedData = id => {
-    const [data, setData] = useState(getSharedData(id))
+    const [data, setData] = useState(typeof window === 'undefined' ? {} : getSharedData(id))
     useEffect(() => {
         return watchSharedData(id, result => {
             setData(result?.data)
